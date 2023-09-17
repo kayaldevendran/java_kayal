95b60151106b49d07836bf5831ea11f84fc47c4c
kayal <kayaldevendran@gmail.com>
2023-09-17 20:43:00 +0530

    alphabet

---
+++ src/java_basics_module1/alphabet.java
@@ -0,0 +1,13 @@
+package java_basics_module1;
+
+public class alphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(char i='A'; i<='Z'; i++)
+{
+System.out.print(i+" ");	
+}
+	}
+
+}
